b9927fbc478f5ee20e97a793825ad4c8138acec9
jamesc-jcu <jamesnapoleon.soechiarto@my.jcu.edu.au>
2025-10-17 14:14:54 +1000

    hello world 1

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    console.log('hello world');
     const conn = await mongoose.connect(
       process.env.MONGO_URI || 'mongodb://localhost:27017/royal-george-hotel'
     );
